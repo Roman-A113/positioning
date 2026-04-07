0414850e1b2a5f1f2f626c244583ec779db848e7
Илья Котов <roskets801@gmail.com>
2026-04-07 19:42:02 +0500

    2

--- index.js
+++ index.js
@@ -5,3 +5,15 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const openBtn = document.getElementById('openBtn');
+const closeBtn = document.getElementById('closeBtn');
+const overlay = document.getElementById('overlay');
+
+openBtn.addEventListener('click', () => {
+    overlay.classList.remove('hidden');
+});
+
+closeBtn.addEventListener('click', () => {
+    overlay.classList.add('hidden');
+});
