3d6da2d8dfdcc31d5ac211e82792e1a8ec51a0e5
Илья Котов <roskets801@gmail.com>
2026-04-07 19:52:27 +0500

    ьук

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/*
-    Изменить элементу цвет и ширину можно вот так:
-
-    const element = document.querySelector('.myElement');
-    element.style.color = 'red';
-    element.style.width = '300px';
-*/
 
 const openBtn = document.getElementById('openBtn');
 const closeBtn = document.getElementById('closeBtn');
