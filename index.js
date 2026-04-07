743e1c848e7a1451cd6ef254a4e36a9dfc06f831
Roman <roman.koles@bk.ru>
2026-04-07 20:21:24 +0500

    done 4

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ openBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     overlay.classList.add('hidden');
 });
+
+
+progress_bar = document.querySelector('.progress-bar-inner');
+setInterval(() => animateProgressBar(), 10);
+
+function animateProgressBar() {
+    const currentWidth = parseFloat(getComputedStyle(progress_bar).width);
+    const parentWidth = parseFloat(getComputedStyle(progress_bar.parentElement).width);
+    const newWidth = Math.min(currentWidth + 1, parentWidth);
+    progress_bar.style.width = newWidth + 'px';
+}
+
